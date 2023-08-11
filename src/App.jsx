866b952e8a60ba56433e00fd50982270437a3dd2
OyindamolaWbCode <kushimo.oyindamola@gmail.com>
2023-08-11 14:29:41 +0100

    img added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import profile from './assets/profilePhotos.png';
+import siG from './assets/slG.png';
 
 function App() {
 
@@ -11,8 +12,31 @@ function App() {
           <p><b>John Doe</b></p>
           </div>
           <div className='profile-dash'>
-
+            <div className='bar'>
+              <p className='bar-para'>Twitter Link</p>
+            </div>
+            <div className='bar'>
+              <p className='bar-para'>KodeCamp Team</p>
+            </div>
+            <div className='bar'>
+              <p className='bar-para'>KodeCamp Books</p>
+            </div>
+            <div className='bar'>
+              <p className='bar-para'>Python Books</p>
+            </div>
+            <div className='bar'>
+              <p className='bar-para'>background Check for Coders</p>
+            </div>
+            <div className='bar'>
+              <p className='bar-para'>Design books</p>
+            </div>
+            <div className='logo'>
+              <img src = {siG} alt='' />
+            </div>
           </div>
+      </div>
+      <div className='flexy-footer'>
+        
       </div>
     </>
   )
